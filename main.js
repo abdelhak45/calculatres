d10af9d7870210f9117178d2a44fa4940e493d1f
abdelhak45 <achtouiabdelhak4@gmail.com>
2023-10-17 11:42:51 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,167 @@
+let inpu1 = document.getElementById('inpu1') 
+let inpu  = document.getElementById('inpu') 
+let inpu2 = document.getElementById('inpu2') 
+
+let result = document.getElementById('result') 
+let alam1 =document.getElementById('alam1')
+let alam2 =document.getElementById('alam2')
+let alam3 =document.getElementById('alam3')
+let alam4 =document.getElementById('alam4')
+
+// ============================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alam1.onclick=()=>{
+//     inpu.value =  alam1.innerHTML
+//     inpu.value = +inpu1.value + +inpu2.value 
+// }
+// alam2.onclick=()=>{
+//     inpu.value =  alam2.innerHTML
+//     inpu.value = +inpu1.value - +inpu2.value 
+// }
+// alam3.onclick=()=>{
+//     inpu.value =  alam3.innerHTML
+//     inpu.value = +inpu1.value * +inpu2.value 
+// }
+// alam4.onclick=()=>{
+//     inpu.value =  alam4.innerHTML
+//     inpu.value = +inpu1.value / +inpu2.value 
+// }
+
+
+
+
+
+
+
+// let num1 = document.getElementById('num1') 
+// let num2 = document.getElementById('num2') 
+// let num3 = document.getElementById('num3') 
+// let num4 = document.getElementById('num4') 
+// let num5 = document.getElementById('num5') 
+// let num6 = document.getElementById('num6') 
+// let num7 = document.getElementById('num7') 
+// let num8 = document.getElementById('num8') 
+// let num9 = document.getElementById('num9') 
+// let num0 = document.getElementById('num0') 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// num.onclick=()=>{
+//     inpu.value =  num1.innerHTML
+// }
+// num1.onclick=()=>{
+//     inpu.value =  num1.innerHTML
+// }
+// num1.onclick=()=>{
+//     inpu.value =  num1.innerHTML
+// }
+// num1.onclick=()=>{
+//     inpu.value =  num1.innerHTML
+// }
+// num1.onclick=()=>{
+//     inpu.value =  num1.innerHTML
+// }
+// num1.onclick=()=>{
+//     inpu.value =  num1.innerHTML
+// }
+// num1.onclick=()=>{
+//     inpu.value =  num1.innerHTML
+// }
+// num1.onclick=()=>{
+//     inpu.value =  num1.innerHTML
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let a = "1,2,3,EE,l,z,r,o,_,W,e,b,S,c,h,o,o,l,2,0,Z"
+
+// let b = a.split("").filter((ele)=>{
+//     return (ele == "," || ele == "_" || !isNaN(parseInt(ele)) )? ele = "":ele;
+    
+// },10)
+// .join("")
+
+
+// console.log(b.length >= 2? b= b.substring(1,b.length-1):false);
+
+
+
+
+
+
+
+
